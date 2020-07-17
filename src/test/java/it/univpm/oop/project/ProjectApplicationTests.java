d3c16e59178feab5d332dbee69d24548abe85f44
Giuseppe Di Mauro <dimaurogiuseppe98@gmail.com>
2020-07-17 23:27:24 +0200

    FINISH

--- src/test/java/it/univpm/oop/project/ProjectApplicationTests.java
+++ src/test/java/it/univpm/oop/project/ProjectApplicationTests.java
@@ -8,32 +8,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import it.univpm.oop.project.application.ProjectApplication;
 import it.univpm.oop.project.controller.Controller;
 import it.univpm.oop.project.exception.FilterException;
-import it.univpm.oop.project.model.Feed;
+import it.univpm.oop.project.model.Comment;
+import it.univpm.oop.project.model.Filter;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes = ProjectApplication.class)
 public class ProjectApplicationTests {
 	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
-
+	
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 	
 	@Test
-	public void contextLoads() {
+	public void NullPointerExTest() {
 		Controller controller = new Controller();
-		assertThrows(FilterException.class, ()-> controller.filteredComments("emo") );
+		assertThrows(NullPointerException.class, ()-> controller.filteredComments("emo") );
 	}
 	
 	@Test
-	public void contextLoads2() {
-		Controller controller = new Controller();
-		Feed feed = new Feed();
-		assertEquals(controller.Feed(), feed);
+	public void CommentTest() {
+		String msg = "test";
+		Comment comment = new Comment();
+		comment.setMessage(msg);
+		assertEquals(msg , comment.getMessage());
 	}
+	
+	@Test
+	public void FilterExTest() {
+		Comment comment = new Comment();
+		
+		//ok
+		assertThrows(FilterException.class, ()-> Filter.detectFilter("emo", comment) );
+		
+		//il filtro passato è corretto, infatti l'eccezione non viene lanciata
+		// e mi dà failure
+		assertThrows(FilterException.class, ()-> Filter.detectFilter("emoticon", comment) );
+	}
+
 }
